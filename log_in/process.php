6dd82c0b4c2758ef154a825825519435cb4f3a6d
PetraAbosi <petra.abosi@ashesi.edu.gh>
2020-11-22 18:57:03 +0000

    connecting data from the frontend to the backend

--- log_in/process.php
+++ log_in/process.php
@@ -5,8 +5,11 @@ if (isset($_POST['register']) ){
   echo 'data recieved';
 
   $fname = $_POST['fname'];
-  $fname = $_POST['fname'];
-  $fname = $_POST['fname'];
+  $midname = $_POST['midname'];
+  $lname = $_POST['lname'];
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+  $password_2 = $_POST['password_2'];
 }
 
  ?>
